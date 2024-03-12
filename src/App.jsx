02f9256ac530a7d0fb3f43a2f9d7767a37d8ff08
asraful613 <atifasraful530@gmail.com>
2024-03-12 15:21:43 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl bg-amber-300'>Knowledge-Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
