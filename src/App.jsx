576b9413899d78a7d12508306c1ba3c80eae7dfc
asraful613 <atifasraful530@gmail.com>
2024-03-12 15:49:38 +0000

    added a blog json file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Component/Header/Header'
+import Blogs from './Component/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
       
-      <h1 className='text-6xl bg-amber-300'>Knowledge-Cafe</h1>
+      <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
