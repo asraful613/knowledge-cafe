c5c591dc20a6cc2ba1c295995043db95ee86d66a
asraful613 <atifasraful530@gmail.com>
2024-03-13 10:49:40 +0000

    added a  google font and tailwind css

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types';
 const Blog = ({blog}) => {
-    console.log(blog);
+    const {title,cover}=blog;
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <h2 className='text-4xl'>{title}</h2>
         </div>
     );
 };
+
+Blog.propTypes={
+    blog:PropTypes.object.isRequired
+}
 
 export default Blog;
